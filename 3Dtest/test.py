835451ba910775ccd8fdb5b71ece9372cfb0611e
toscoding <tim.jeu.ecole@gmail.com>
2018-05-04 22:01:48 +0200

    P5.js

--- 3Dtest/test.py
+++ 3Dtest/test.py
@@ -6,16 +6,19 @@ import time
 #turtle.speed(10)
 #turtle.ht()
 
-h=0
+h=95
 u=0
 g=100
 m=0
-width=30
-s=0.00833333333
+width=20
+s=1/500000000
 x = 0
 y = 0
 z = 0
-
+ma= atan(1/sqrt(2))
+lenght=30
+o=scene.camera.pos
+print (o)
 
 def stick() :
     global h
@@ -32,28 +35,32 @@ def stick() :
     turtle.left(90)
     turtle.clear()
 def d() :
-    global h, y, width
+    global h, y,z, width, lenght
+
     y=-5
+    z=0
     m=0
     po = vector(0, -5, 0)
-    b = box(pos=po, length=40, height=h, width=width, color=color.blue)
+    b = box(pos=po, length=lenght, height=h, width=width, color=color.blue)
     b.visible = True
     time.sleep(s)
     b.visible = False
-    for m in range (10) :
-        po = vector(0, -5, y)
-        bm = b.clone(pos=pom)
-        bm.visible = True
-        time.sleep(s)
-        bm.visible = False
-        del bm
-        y = y + width
+    for ma in range (5) :
+        z=z+lenght + 2
+        y=-5
+        for m in range (10) :
+            po = vector(z, -5, y)
+            bm = b.clone(pos=po)
+            bm.visible = True
+            time.sleep(s)
+            bm.visible = False
+            del bm
+
+            y = y + width + 2
+
     del b
 
-
-
 while True :
-
 
     while (h != g) :
         h=h+5
@@ -66,7 +73,8 @@ while True :
             h=19
 
     while (h != 5) :
-
+        #scene.delete()
+        #scene = canvas(width=500, height=500)
         u=u+1
         h=h-5
         #box(pos=po, length=40, height=h, width=30, color=color.blue)
---
+++ P5/1/sketch.js
@@ -0,0 +1,35 @@
+
+let v = 0;
+let w = 30;
+let ma;
+let max;
+
+function setup() {
+  createCanvas(400, 400, WEBGL);
+  ma = atan(cos(QUARTER_PI));
+  max = dist(0, 0, 200, 200);
+}
+
+function draw() {
+  background(100);
+  ortho(-200, 200, 200, -200, 0, 1000);
+  rotateX(-ma);
+  rotateY(-QUARTER_PI)﻿
+
+  for (let z = 0; z < height; z += w) {
+    for (let x = 0; x < width; x += w) {
+      push();
+      let d = dist(x, z, width / 2, height / 2);
+      let offset = map(d, 0, max, -PI, PI);
+      let a = v + offset;
+      let h = floor(map(sin(a), -1, 1, 100, 300));
+      translate(x - width / 2, 0, z - height / 2);
+      normalMaterial();
+      box(w, h, w);
+      //rect(x - width / 2 + w / 2, 0, w - 2, h);
+      pop();
+    }
+  }
+
+  v -= 0.1;
+}
